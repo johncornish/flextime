6143418fd591b169f27d9f2e9712c71cd5a0743c
John Cornish <cornish@mymail.mines.edu>
2017-10-06 14:31:25 -0600

    Apply leaf transformations easily, order by multiple keys

--- ftime.py
+++ ftime.py
@@ -1,19 +1,47 @@
-import yaml, re
+import yaml, re, dateutil.parser
+from functools import reduce
 
-def leaves(data, names = [], attributes = {}):
-    children = [(k,v) for k, v in data.items() if not re.match("^_.*", str(k))]
-    these_attributes = {k: v for k, v in data.items() if re.match("^_.*", str(k))}
+def str_to_date(leaf):
+    attrs = leaf.get('attrs')
+    if '_d' in leaf['attrs']:
+        attrs['_d'] = dateutil.parser.parse(attrs.get('_d'))
 
-    if len(children) > 0:
-        for name, c in children:
-            yield from leaves(c, names + [name], dict(attributes, **these_attributes))
-    else:
-        yield (' '.join([str(n) for n in names]), dict(attributes, **these_attributes))
+    return dict(leaf, attrs = attrs)
 
+def add_ordinality(leaf):
+    ordattrs = dict(leaf.get('attrs'))
+    if '_d' in ordattrs:
+        ordattrs.update({'_d': dateutil.parser.parse(ordattrs.get('_d')).toordinal()})
+
+    return dict(leaf, ordattrs = ordattrs)
+
+class FlexTime:
+    def extract_leaves(self):
+        def rfind_leaves(data, names = [], attributes = {}):
+            children = [(k,v) for k, v in data.items() if not re.match("^_.*", str(k))]
+            these_attributes = {k: v for k, v in data.items() if re.match("^_.*", str(k))}
+
+            if len(children) > 0:
+                for name, c in children:
+                    yield from rfind_leaves(c, names + [name], dict(attributes, **these_attributes))
+            else:
+                yield {'names': names, 'attrs': dict(attributes, **these_attributes)}
+
+        self.leaves = list(rfind_leaves(self.dtree))
+
+    def mmap(self, *funcs):
+        def apply_func(acc, f):
+            return map(f, acc)
+        
+        return list(reduce(apply_func, funcs, self.leaves))
+        
+    def __init__(self, data_file_name):
+        with open(data_file_name) as f:
+            self.dtree = yaml.safe_load(f)
+            self.extract_leaves()
+
+    
 def multisort(lvs, *keys):
-    return sorted(lvs, key = lambda x: [x[1].get(attr, 0) for attr in keys])
+    return sorted(lvs, key = lambda x: [x['ordattrs'].get(attr, 0) for attr in keys])
 
-with open('data.yml') as f:
-    d = yaml.safe_load(f)
-    lvs = leaves(d)
-    [print(i) for i in list(multisort(lvs, '_t', '_d'))]
+[print(l) for l in multisort(FlexTime('data.yml').mmap(add_ordinality), '_d', '_t')]
