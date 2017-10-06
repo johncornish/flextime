944b76dce21c42abb98a2f825c1c91493e9d5c9d
John Cornish <cornish@mymail.mines.edu>
2017-10-06 11:06:12 -0600

    Update children with missing attributes from parent

--- ftime.py
+++ ftime.py
@@ -1,12 +1,14 @@
 import yaml, re
 
-def leaves(data, names = []):
+def leaves(data, names = [], attributes = {}):
     children = [(k,v) for k, v in data.items() if not re.match("^_.*", str(k))]
+    these_attributes = {k: v for k, v in data.items() if re.match("^_.*", str(k))}
+
     if len(children) > 0:
         for name, c in children:
-            yield from leaves(c, names + [name])
+            yield from leaves(c, names + [name], dict(attributes, **these_attributes))
     else:
-        yield (' '.join([str(n) for n in names]), data)
+        yield (' '.join([str(n) for n in names]), dict(attributes, **these_attributes))
 
 with open('data.yml') as f:
     d = yaml.safe_load(f)
