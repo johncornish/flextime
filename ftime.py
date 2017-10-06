5b5890e84e0c491346b1dc2fae7004ea135d1206
John Cornish <cornish@mymail.mines.edu>
2017-10-06 14:42:09 -0600

    Friendlier output

--- ftime.py
+++ ftime.py
@@ -1,12 +1,12 @@
 import yaml, re, dateutil.parser
 from functools import reduce
 
-def str_to_date(leaf):
+def expand_date(leaf):
     attrs = leaf.get('attrs')
     if '_d' in leaf['attrs']:
-        attrs['_d'] = dateutil.parser.parse(attrs.get('_d'))
+        attrs['_d'] = dateutil.parser.parse(attrs.get('_d')).strftime("%m-%d-%Y")
 
-    return dict(leaf, attrs = attrs)
+    return leaf
 
 def add_ordinality(leaf):
     ordattrs = dict(leaf.get('attrs'))
@@ -44,4 +44,5 @@ class FlexTime:
 def multisort(lvs, *keys):
     return sorted(lvs, key = lambda x: [x['ordattrs'].get(attr, 0) for attr in keys])
 
-[print(l) for l in multisort(FlexTime('data.yml').mmap(add_ordinality), '_d', '_t')]
+[print("{} - {} minutes".format(' '.join([str(n) for n in l.get('names')]),
+                                l['attrs'].get('_t', 0))) for l in multisort(FlexTime('data.yml').mmap(add_ordinality, expand_date), '_d', '_t')]
