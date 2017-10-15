37bd16494f0259a5c0354e39499a0edf6acd7874
John Cornish <cornish@mymail.mines.edu>
2017-10-14 22:22:44 -0600

    Extended TaskTree

--- flextime/tasktree.py
+++ flextime/tasktree.py
@@ -1,6 +1,44 @@
-import yaml
+import yaml, re, dateutil.parser
 from os.path import isfile
+from datetime import datetime
+from functools import reduce
 
+class TaskLeaf:
+    def __init__(self, path, data):
+        self.path = path
+        self.data = data
+
+    def __str__(self):
+        date_format = "%m-%d-%y"
+        if '_d' in self.data:
+            due_date = dateutil.parser.parse(self.data.get('_d'))
+        else:
+            due_date = datetime.today()
+
+        return '({}) {} - {}'.format(
+            due_date.strftime(date_format),
+            ' | '.join([str(n) for n in self.path]),
+            self.data.get('_t', 0)
+        )
+
+    def toordinal(self, attrs):
+        def get_ord(attr):
+            val = 0
+            attr_key = '_' + re.sub('^', '', attr)
+            if attr_key in self.data:
+                if attr_key == '_d':
+                    val = dateutil.parser.parse(self.data.get('_d')).toordinal()
+                else:
+                    val = self.data.get(attr_key)
+
+                if re.match('^\^.*', str(attr)):
+                    val = -val
+
+            return val
+            
+        return list(map(get_ord, attrs))
+
+    
 class TaskTree:
     def __init__(self, datafile=False):
         if not datafile:
@@ -12,17 +50,46 @@ class TaskTree:
             with open(self._datafile) as f:
                 self._datatree = yaml.safe_load(f)
         else:
-            print("I will write to {} on save".format(self._datafile))
+            print("{} not found. It will be created on save.".format(self._datafile))
             self._datatree = {}
 
     def save(self):
-        output = yaml.dump(self._datatree)
+        output = self.dump()
         with open(self._datafile, 'w') as f:
             f.write(output)
 
-    def add_branch(self, path, data):
+    def dump(self):
+        return yaml.dump(self._datatree)
+
+    def leaves(self):
+        def recursive_find(data, path = [], attrs = {}):
+            children = [(k,v) for k, v in data.items() if not re.match("^_.*", str(k))]
+            new_attrs = dict(attrs, **{k: v for k, v in data.items() if re.match("^_.*", str(k))})
+
+            if len(children) > 0:
+                for key, child in children:
+                    yield from recursive_find(child, path + [key], new_attrs)
+            else:
+                yield TaskLeaf(path, new_attrs)
+
+        return list(recursive_find(self._datatree))
+
+    def keys_from_path(self, path):
+        return list(self.branch_from_path(path).keys())
+                    
+    def merge_branch(self, path, data):
         node = self.branch_from_path(path)
         node.update(data)
+
+    def delete_branch(self, path):
+        node = self.branch_from_path(path[:-1])
+        if len(path) >= 1:
+            delete_key = path[-1]
+            if delete_key in node:
+                del node[delete_key]
+            else:
+                print("{} not found in tree:".format(' > '.join(map(str, path))))
+                print(self.dump())
 
     def branch_from_path(self, path):
         node = self._datatree
