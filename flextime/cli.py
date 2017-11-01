f056d56c277fbbc7dcce1643a7b9b757a0217113
John Cornish <cornish@mymail.mines.edu>
2017-11-01 09:20:31 -0600

    Rudimentary documentation for commands

--- flextime/cli.py
+++ flextime/cli.py
@@ -12,6 +12,7 @@ def cli(ctx, datafile):
 @click.argument('sort_keys', nargs=-1)
 @click.pass_obj
 def list(obj, sort_keys):
+    """Display a sorted list of tasks."""
     List(obj['tasktree'], sort_keys).run()
 
 @cli.command()
@@ -19,12 +20,14 @@ def list(obj, sort_keys):
 @click.option('-m', '--merge-files', multiple=True)
 @click.pass_obj
 def add(obj, path, merge_files):
+    """Add to an existing tree. Guess path in tree if arguments provided."""
     Add(obj['tasktree'], path, merge_files).run()
 
 @cli.command()
 @click.argument('schedule_file', default='schedule.yml')
 @click.pass_obj
 def show(obj, schedule_file):
+    """Attempt to optimally schedule tasks against blocks of time configured in a schedule file."""
     s = flextime.Scheduler(obj['tasktree'], schedule_file)
     unscheduled, tbs = s.scheduled_tasks()
     
