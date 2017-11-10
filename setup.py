13eb9d6ba2e7602c30a2baef7fd07580bf3d72b5
John Cornish <cornish@mymail.mines.edu>
2017-11-10 10:53:49 -0700

    Let's put this on PyPi!

--- setup.py
+++ setup.py
@@ -2,8 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='flextime',
-    version='0.1',
+    version='1.0',
+    license='GPLv3',
     packages=['flextime'],
+    description='Hierarchical task management tool',
+    author='John Frederick Cornish IV',
+    author_email='johncornishthe4th@gmail.com',
+    url='https://github.com/johncornish/flextime',
+    download_url='https://github.com/johncornish/flextime/archive/1.0.tar.gz',
     install_requires=[
         'click',
         'python-dateutil',
